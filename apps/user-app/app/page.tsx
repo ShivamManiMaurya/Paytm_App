bfec4984ae2c73ae9a046c504cb2bc431e19fb9e
Shivam Maurya <shivammaurya097@gmail.com>
2025-05-29 21:12:53 +0530

    init: zustand

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,9 +4,12 @@ import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 import { useState } from "react";
+import { useWalletStore } from "@repo/store";
 
 export default function Home() {
   const [message, setMessage] = useState("");
+  const balance = useWalletStore((s) => s.balance);
+  const updateBalance = useWalletStore((s) => s.updateBalance);
 
   const callApi = async () => {
     const res = await fetch("/api/users"); // API route
@@ -21,6 +24,10 @@ export default function Home() {
       </h1>
       <button onClick={callApi}>Call API</button>
       {message && <p>{message}</p>}
+      <div>
+        <h2>Balance: ₹{balance}</h2>
+        <button onClick={() => updateBalance(balance + 100)}>Add ₹100</button>
+      </div>
     </>
   );
 }
