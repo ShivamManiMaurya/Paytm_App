9476d2eb210e5810687373945d6d840fe873736b
Shivam Maurya <shivammaurya097@gmail.com>
2025-07-08 22:43:59 +0530

    fix: building errors

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,23 +1,6 @@
-"use client";
-
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-import { useState } from "react";
-import { useWalletStore } from "@repo/store";
 import LandingPage from "./pages/landingPage";
 
 export default function Home() {
-  const [message, setMessage] = useState("");
-  const balance = useWalletStore((s) => s.balance);
-  const updateBalance = useWalletStore((s) => s.updateBalance);
-
-  const callApi = async () => {
-    const res = await fetch("/api/users"); // API route
-    const data = await res.json();
-    setMessage(data.message);
-  };
-
   return (
     <>
       <LandingPage />
