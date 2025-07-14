deaae081e7067bbd1b5bb39f0f3bb4eda59ecf20
Shivam Maurya <shivammaurya097@gmail.com>
2025-07-14 22:32:52 +0530

    fix

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -3,12 +3,24 @@ import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
+    // Add Prisma plugin for server
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()];
     }
+
+    // Add workspace aliases
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@repo/store": "../../packages/store/src",
+      "@repo/ui": "../../packages/ui/src",
+    };
+
     return config;
   },
   output: "standalone",
+  experimental: {
+    externalDir: true, // Allows importing from outside the app directory
+  },
 };
 
 export default nextConfig;
